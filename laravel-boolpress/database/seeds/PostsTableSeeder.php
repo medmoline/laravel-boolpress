8c5cf6de39e8cf2635578a6859cdea9bbd2eaeaa
med_moline <antonio.medugno1990@gmail.com>
2019-07-18 16:28:57 +0200

    Faker per database + HomeController per la home pubblica

---
+++ laravel-boolpress/database/seeds/PostsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+//per includere i faker all'interno del seed
+use Faker\Factory as Faker;
+//includo la classe Post
+use App\Post;
+use Illuminate\Support\Str;
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        for ($i=0; $i < 20 ; $i++) {
+          $new_post = new Post();
+          $new_post->title = $faker->sentence();
+          $new_post->content = $faker->text(200);
+          $new_post->author = $faker->name;
+          $new_post->slug = Str::slug($new_post->title);
+          $new_post->save();
+        }
+    }
+}
